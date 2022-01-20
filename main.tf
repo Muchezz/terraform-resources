e4179c8061302768b8eb4775037b416f4dad8552
Mucheru <mucheru@outlook.com>
2022-01-20 17:42:15 +0300

    Adding a route table association

--- main.tf
+++ main.tf
@@ -46,3 +46,10 @@ resource "aws_route" "dev_public_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.dev_igw.id
 }
+
+
+#Route the subnet to the route table
+resource "aws_route_table_association" "dev_public_route_table_association" {
+  subnet_id      = aws_subnet.main_public_subnet.id
+  route_table_id = aws_route_table.dev_public_route_table.id
+}
