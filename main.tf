fcff983ab71429054e19148fce3e4d8f1d2a875b
Mucheru <mucheru@outlook.com>
2022-01-20 17:00:24 +0300

    Creating a subnet

--- main.tf
+++ main.tf
@@ -9,3 +9,17 @@ resource "aws_vpc" "main_vpc" {
   }
   
 }
+
+#Create a subnet in the VPC
+
+resource "aws_subnet" "main_public_subnet" {
+  vpc_id = "${aws_vpc.main_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-2a"
+  
+  tags = {
+    Name = "terraform-dev-public-subnet"
+  }
+  
+}
